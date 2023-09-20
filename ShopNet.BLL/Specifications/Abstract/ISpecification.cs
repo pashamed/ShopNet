9b7c69f6cb0b38600240f8a139203108e83b6842
PashMed <pashamedenets@gmail.com>
2023-09-20 16:53:41 +0300

    add angular client

--- ShopNet.BLL/Specifications/Abstract/ISpecification.cs
+++ ShopNet.BLL/Specifications/Abstract/ISpecification.cs
@@ -4,7 +4,7 @@ namespace ShopNet.BLL.Specifications.Abstract
 {
     public interface ISpecification<T>
     {
-        Expression<Func<T, bool>>? Criteria { get; }
+        Expression<Func<T, bool>> Criteria { get; }
         List<Expression<Func<T, object>>> Includes { get; }
         Expression<Func<T, object>> OrderBy { get; }
         Expression<Func<T, object>> OrderByDescending { get; }
