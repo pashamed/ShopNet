1564bd4f187169effcae6f4e8c3aeff7cf585d20
PashMed <pashamedenets@gmail.com>
2023-09-22 12:57:13 +0300

    add header, display products components

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,8 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Product } from './models/product';
-import { Pagination } from './models/pagination';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,21 +6,6 @@ import { Pagination } from './models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'ShopNet';
-  products: Product[] = [];
 
-  constructor(private httpClient: HttpClient) {}
-
-  ngOnInit(): void {
-    this.httpClient
-      .get<Pagination<Product[]>>(
-        'https://localhost:5001/api/products?pageSize=50'
-      )
-      .subscribe({
-        next: (response) => (this.products = response.data),
-        error: (error) => console.log(error),
-        complete: () => {
-          console.log('request completed');
-        },
-      });
-  }
+  ngOnInit(): void {}
 }
