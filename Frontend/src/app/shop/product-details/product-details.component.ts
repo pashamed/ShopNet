b35d3ce954785aba399213d3e13c007a2461d18e
PashMed <pashamedenets@gmail.com>
2023-09-26 13:50:09 +0300

    add breadcrumbs

--- Frontend/src/app/shop/product-details/product-details.component.ts
+++ Frontend/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ShopService } from '../shop.service';
 import { Product } from 'src/app/shared/models/product';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -13,7 +14,8 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(
     private shopService: ShopService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private bcService: BreadcrumbService
   ) {}
 
   ngOnInit(): void {
@@ -24,7 +26,11 @@ export class ProductDetailsComponent implements OnInit {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     if (id)
       this.shopService.getProduct(+id).subscribe({
-        next: (product) => (this.product = product),
+        next: (product) => {
+          this.product = product;
+          this.bcService.set('shop/:id', product.name);
+          //this.bcService.set('@productDetails', product.name);
+        },
         error: (err) => console.log(err),
       });
   }
