dbdd8f398c83e247a3af98b035a0e44917e0de54
pashamed <pashamedenets@gmail.com>
2023-10-03 12:36:46 +0300

    add token service

---
+++ ShopNet.BLL/Interfaces/IToketService.cs
@@ -0,0 +1,9 @@
+﻿using ShopNet.DAL.Entities.Identity;
+
+namespace ShopNet.BLL.Interfaces
+{
+    public interface IToketService
+    {
+        string CreateToken(AppUser user);
+    }
+}
---
+++ ShopNet.BLL/Services/TokenService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using ShopNet.BLL.Interfaces;
+using ShopNet.DAL.Entities.Identity;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ShopNet.BLL.Services
+{
+    public class TokenService: IToketService
+    {
+        private readonly IConfiguration config;
+        private readonly SymmetricSecurityKey key;
+
+        public TokenService(IConfiguration config)
+        {
+            this.config = config;
+            key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Token:Key"]));
+        }
+
+        public string CreateToken(AppUser user)
+        {
+            var claims = new List<Claim> 
+            {
+                new Claim(ClaimTypes.Email,user.Email),
+                new Claim(ClaimTypes.GivenName, user.DisplayName)
+            };
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(int.Parse(config["Token:Expiry"])),
+                SigningCredentials = credentials,
+                Issuer = config["Token:Issuer"]
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+            
+        }
+    }
+}
