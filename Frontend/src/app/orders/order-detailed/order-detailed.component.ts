026e20c2f8a26b11a71ff86439431e1ff9a7d15e
pashamed <pashamedenets@gmail.com>
2023-10-26 15:41:06 +0300

    fix: order num breadcrumb

--- Frontend/src/app/orders/order-detailed/order-detailed.component.ts
+++ Frontend/src/app/orders/order-detailed/order-detailed.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Order } from 'src/app/shared/models/order';
 import { OrdersService } from '../orders.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-order-detailed',
@@ -13,12 +14,17 @@ export class OrderDetailedComponent implements OnInit {
 
   constructor(
     private orderService: OrdersService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private breadCrumbService: BreadcrumbService
   ) {}
   ngOnInit(): void {
     this.getOrder(<string>this.route.snapshot.paramMap.get('id'));
   }
   async getOrder(id: string) {
     this.order = await this.orderService.getOrder(id);
+    this.breadCrumbService.set(
+      'orders/:id',
+      `Order #${this.order.id}  ${this.order.status}`
+    );
   }
 }
